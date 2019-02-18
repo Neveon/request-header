cba55c6f0ff69c768d1a4f55b53652342afc7150
Neil <neiltz711@gmail.com>
2019-02-18 13:47:53 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -20,22 +20,19 @@ app.use(function(req, res, next){
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-//The ISO 8601 syntax (YYYY-MM-DD) is the preferred JavaScript date format:
-app.get('/api/whoami/:date_string?', (req, res) => {
+app.get('/api/whoami/', (req, res) => {
   //req.get() Returns the specified HTTP request header field
   let software = req.get('User-Agent'); //software used by client
   let language = req.get('Accept-Language'); //language used by client
   let ip = req.ip; //client ip
-  //console.log(date_string);
   res.json({"ipaddress":ip,"language":language,
 "software":software});
   
 });
 
 /** Redirecting home to api/whoami */
-app.get('/', (req, res, /*next*/) => {
+app.get('/', (req, res) => {
   res.redirect('/api/whoami/');
-  //next();
 });
 
 app.listen(process.env.PORT || 3000 );
